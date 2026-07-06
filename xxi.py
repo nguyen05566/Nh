2a4c57ecd34721543d696e68b1f0313eff5dd1ff
nguyen05566 <javjp606@gmail.com>
2026-07-06 21:44:01 +0700

    Update xxi.py

--- xxi.py
+++ xxi.py
@@ -41,7 +41,7 @@ CURRENT_PLAYER_ID = int(os.environ.get('GAMEVH_PLAYER_ID', '65692738'))
 TOKEN = 0 
 GAME_ID = os.environ.get('GAMEVH_GAME_ID', 'xiangqi')
 PLACE_PATH = os.environ.get('GAMEVH_PLACE_PATH', 'Lobby.xiangqi.0')
-BOT_DEPTH = int(os.environ.get('BOT_DEPTH', '20'))
+BOT_DEPTH = int(os.environ.get('BOT_DEPTH', '10'))
 
 # ===== CẤU HÌNH TẠO BÀN =====
 BOT_BET_AMT_ID = int(os.environ.get('BOT_BET_AMT_ID', '5'))
